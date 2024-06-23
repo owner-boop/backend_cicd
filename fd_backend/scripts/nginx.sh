af3cf85925c0ec82e99f734f502ea85f0ee6b6cc
owner-boop <alihaider191919a@gmail.com>
2024-06-23 16:00:53 +0500

    Update nginx.sh

--- fd_backend/scripts/nginx.sh
+++ fd_backend/scripts/nginx.sh
@@ -1,12 +1,12 @@
 
-#!/usr/bin/bash
+# !/usr/bin/bash
 
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/fd_backend/nginx/nginx.conf /etc/nginx/sites-available/frauddetectionbackend
-#sudo ln -s /etc/nginx/sites-available/frauddetectionbackend /etc/nginx/sites-enabled/
-sudo ln -s /etc/nginx/sites-available/frauddetectionbackend /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/frauddetectionbackend /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/frauddetectionbackend /etc/nginx/sites-enabled
 #sudo nginx -t
 netstat -tnlp | grep -w 80
 netstat -ano|grep 80|grep LISTEN
