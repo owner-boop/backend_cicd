7ed23a9e61beef30a70984bfa114357d8abedba0
owner-boop <alihaider191919a@gmail.com>
2024-06-26 00:17:21 +0500

    Update views.py

--- fd_backend/fraudapp/views.py
+++ fd_backend/fraudapp/views.py
@@ -577,6 +577,11 @@ class FrequencyAPIView(APIView):
 class multigraphsAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
+        session = boto3.Session(
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    region_name=settings.AWS_REGION
+        )
         s3 = boto3.client('s3')
         bucket_name = 'fraud-detection-esse'
         key = 'dataset_with_labels.csv'
